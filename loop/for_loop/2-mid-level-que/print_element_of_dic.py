a62e1792152d2e85a2ac2acf32a6083a0308b1d2
purvesh-pokale <pokalepurvesh2003@gmail.com>
2026-03-18 11:21:10 -0700

    print element from dictionary

---
+++ loop/for_loop/2-mid-level-que/print_element_of_dic.py
@@ -0,0 +1,16 @@
+#Print all elements of a dictionary using a loop.
+d={"name" : "purvesh",
+   "age" : 21,
+   "city" : "pune"
+   }
+
+for k , v in d.items():
+    print(k,"=",v)
+
+'''
+o/p
+
+name = purvesh
+age = 21
+city = pune
+'''
