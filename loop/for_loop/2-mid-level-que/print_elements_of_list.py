0a5cfe71491745827ca2840c1baeaa525bf79917
purvesh-pokale <pokalepurvesh2003@gmail.com>
2026-03-18 00:01:32 -0700

    print all element of list by using for loop and while loop

---
+++ loop/for_loop/2-mid-level-que/print_elements_of_list.py
@@ -0,0 +1,14 @@
+#Print all elements of a list using a for loop.
+l=[10,20,30,40,50]
+for i in l:
+    print(i)
+print()
+
+# Print all elements of a list using a while loop.
+l=[10,20,30,40,50]
+i=0
+while i<len(l):
+    print(l[i])
+    i+=1
+
+print()
