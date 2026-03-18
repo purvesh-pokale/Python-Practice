5a5c8550a6f401b56432ad4bae2c604dac5d7912
purvesh-pokale <pokalepurvesh2003@gmail.com>
2026-03-17 21:23:00 -0700

    Reverse the order of list

---
+++ loop/for_loop/2-mid-level-que/rev_list.py
@@ -0,0 +1,11 @@
+#Print a list of numbers in reverse order using a loop.
+list1=[20,30,40,50,60]
+list2=[]
+for i in list1:
+    list2=[i]+list2
+
+print("Reverse order list :",list2)
+'''
+o/p
+
+'''
