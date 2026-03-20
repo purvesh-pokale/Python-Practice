f72bde2cd16d674ab50312bee1aaad69faa294e3
purvesh-pokale <pokalepurvesh2003@gmail.com>
2026-03-20 11:30:29 -0700

    count occurence of each character in string

---
+++ loop/for_loop/2-mid-level-que/count_each_cha.py
@@ -0,0 +1,40 @@
+# count occurence of each character in string
+# Write a program to count occurrence of each character in the word "programing"
+# input : "programing"
+# Expected output
+'''
+p : 1
+r : 2
+o : 1
+g : 2
+a : 1
+m : 1
+i : 1
+n : 1
+'''
+
+word = "programing"
+
+char_count ={}
+
+for i in word:
+    if i in char_count:
+        char_count[i] +=1
+
+    else:
+        char_count [i] = 1
+
+for char, count in char_count.items():
+    print(char , ":" , count)
+
+'''
+o/p
+p : 1
+r : 2
+o : 1
+g : 2
+a : 1
+m : 1
+i : 1
+n : 1
+'''
