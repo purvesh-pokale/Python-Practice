9036ff4632a3aedb3588ec34483ad387e8f8a392
purvesh-pokale <pokalepurvesh2003@gmail.com>
2026-03-25 10:45:08 -0700

    5x5 square stare pattern

---
+++ loop/nested-loop/star-pattern/qu1.py
@@ -0,0 +1,13 @@
+"""
+qu1. Print this pattern
+* * * * *
+* * * * *
+* * * * *
+* * * * *
+* * * * *
+"""
+
+for i in range(1,6):
+    for j in range(1,6):
+        print("*",end=" ")
+    print()                # if we dident give the print() it not enter in next line
