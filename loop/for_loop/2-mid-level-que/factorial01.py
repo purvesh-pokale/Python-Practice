778f27550b7bf91445bbf127d2ed1fbf35366d6d
purvesh-pokale <pokalepurvesh2003@gmail.com>
2026-07-07 16:11:25 +0530

    Print the table of given number.

--- loop/for_loop/2-mid-level-que/factorial01.py
+++ loop/for_loop/2-mid-level-que/factorial01.py
@@ -2,12 +2,12 @@
 
 num = int(input("Enter the number :"))
 
-pro = 1 
+fact = 1 
 
 for i in range(1,num+1):
-    pro = pro*i
+    fact = fact*i
 
-print(f"the factorial of the {num} is {pro}")
+print(f"the factorial of the {num} is {fact}")
 
 '''
 Enter the number :2
@@ -15,4 +15,7 @@ Enter the number :2
 
  Enter the number :3
  the factorial of the 3 is 6
+
+ Enter the number :4
+the factorial of the 4 is 24
 '''
