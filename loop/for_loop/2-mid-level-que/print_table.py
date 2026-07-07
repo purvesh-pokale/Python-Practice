32e52b2b91470378d8e9475bad5829b5cede3424
purvesh-pokale <pokalepurvesh2003@gmail.com>
2026-07-07 10:25:14 +0530

    Print the table of given number.

---
+++ loop/for_loop/2-mid-level-que/print_table.py
@@ -0,0 +1,25 @@
+# Take a number as input and print its table.
+
+num = int(input("Enter the number :"))
+
+print(f"the table of {num} :")
+for i in range(1,11):
+    print(f"{num} x {i} = {num*i}")
+
+
+'''
+o/p
+
+Enter the number :5
+the table of 5 :
+5 x 1 = 5
+5 x 2 = 10
+5 x 3 = 15
+5 x 4 = 20
+5 x 5 = 25
+5 x 6 = 30
+5 x 7 = 35
+5 x 8 = 40
+5 x 9 = 45
+5 x 10 = 50
+'''
