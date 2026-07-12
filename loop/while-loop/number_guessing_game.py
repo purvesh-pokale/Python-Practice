7e306cf46463e6668746275b55a47f9ee72be215
purvesh-pokale <pokalepurvesh2003@gmail.com>
2026-07-12 16:32:33 +0530

    random number gussing game with the help of 'random library' and how many tries you get to guess

--- loop/while-loop/number_guessing_game.py
+++ loop/while-loop/number_guessing_game.py
@@ -4,7 +4,7 @@ import random
 
 n = random.randint(1,10)
 
-guess = int(input("Please guss your number between 1 and 10 :"))
+guess = int(input("Please guess your number between 1 and 10 :"))
 
 if n == guess:
     print("you are right")
@@ -19,7 +19,47 @@ Please guss your number between 1 and 10 :5
 you are right
 The number is  5
 
-Please guss your number between 1 and 10 :8
-sorry your guess is wrong.
-The number is  1
+'''
+
+
+#tell the how many tries you take to guess
+import random
+
+n = random.randint(1,10)
+
+
+tries = 0
+
+while True:
+    guess = int(input("Please guess your number between 1 and 10 :"))
+
+    if n == guess:
+        tries +=1
+        print(f"Your are right you guessed the number is {tries} tries")
+        break
+
+    elif  n < guess :
+        print("Please go lower.")
+        tries +=1
+    
+    elif  n < guess :
+        print("Please go higher.")
+        tries +=1
+
+    else:
+        print("sorry your guess is wrong.")
+        tries +=1
+
+
+'''
+o/p
+Please guess your number between 1 and 10 :8
+you are right
+Your are right you guessed the number is 1 tries
+
+
+Please guess your number between 1 and 10 :6
+Please go lower.
+Please guess your number between 1 and 10 :5
+Your are right you guessed the number is 2 tries
 '''
