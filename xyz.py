c91a010c48e260de70ea71b400802c30897fbb73
purvesh-pokale <pokalepurvesh2003@gmail.com>
2026-07-17 12:36:49 +0530

    add new programs

--- xyz.py
+++ xyz.py
@@ -4,23 +4,9 @@ s1 = s.split()
 
 
 for i in s1:
+    print(i)
     
-    rev = ""
-    for j in i:
-        rev = j + rev
-    if rev == i:
-        print(i)
 
-
-s =" I am 25 year old my salary is 58000"
-
-sum= 0
-
-for i in s:
-    if i.isdigit():
-        sum = sum+int(i)
-
-print(sum)
          
         
 
